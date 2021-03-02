c605b7b4ee2b0a3cb5b86c7a9588a7862bd55d51
nicholaskevs <nekoman.hunter.tech@gmail.com>
2021-03-02 15:02:05 +1100

    Add buffer to cmd windows

--- silentbot.bat
+++ silentbot.bat
@@ -1,5 +1,6 @@
 @echo off
 mode con: cols=120 lines=30
+powershell -command "&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.width=120;$B.height=1000;$W.buffersize=$B;}"
 title SilentBot
 
 php silentbot.php
